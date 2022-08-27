987250db4067792e98bdc946582b7220ac2d0901
Abc_Programmer <112259649+AbcProgrammer123@users.noreply.github.com>
2022-08-27 19:12:39 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,9 @@
+# Kernel Commands
+
+# 1 = pwd -> print working directory - folder
+# 2 = ls -> list of files and folders
+# 3 = clear -> clean everything
+# 4 = cd -> change folder 
+# 5 = cd .. -> back folder
+# 6 = create new file and folder -> file - touch_____ , folder - mkdir_____
+# 7 = delete file and folder -> file - rm___ , folder - rm -rf_____
